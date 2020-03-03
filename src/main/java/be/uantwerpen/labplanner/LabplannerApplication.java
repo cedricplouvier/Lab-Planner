d58e21b40ec3da962fda5e7e195e9dc7a0b09933
cedricplouvier <cedric.plouvier@student.uantwerpen.be>
2020-03-03 14:48:33 +0100

    Initial commit

---
+++ src/main/java/be/uantwerpen/labplanner/LabplannerApplication.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.labplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabplannerApplication.class, args);
+    }
+
+}
