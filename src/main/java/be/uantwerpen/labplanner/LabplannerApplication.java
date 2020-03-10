4cae0c37284aca5b924f198cab80615f4e3a53d4
Timo <timo.nelen@student.uantwerpen.be>
2020-03-11 00:01:54 +0100

    created DatabaseLoader

--- src/main/java/be/uantwerpen/labplanner/LabplannerApplication.java
+++ src/main/java/be/uantwerpen/labplanner/LabplannerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
 public class LabplannerApplication {
@@ -15,7 +16,9 @@ public class LabplannerApplication {
 
     @RequestMapping({"/","/home"})
     public String showHomepage(){
-        return "StockPage";
+        return "product-list";
     }
+
+
 
 }
