3d1bef317cd4801aa98632b0367fb39cd1ea3936
RubenJoosen <38206670+RubenJoosen@users.noreply.github.com>
2020-03-24 16:20:43 +0100

    unique name of Role, User & Privilege fixed.

--- src/main/java/be/uantwerpen/labplanner/Controller/PrivilegeController.java
+++ src/main/java/be/uantwerpen/labplanner/Controller/PrivilegeController.java
@@ -5,9 +5,11 @@ import be.uantwerpen.labplanner.Service.OwnPrivilegeService;
 import be.uantwerpen.labplanner.common.model.users.Privilege;
 import be.uantwerpen.labplanner.common.service.users.PrivilegeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,24 +23,31 @@ public class PrivilegeController {
     @Autowired
     private OwnPrivilegeService privilegeService;
 
+    @ModelAttribute("allPrivileges")
+    public Iterable<Privilege> populatePrivileges(){return privilegeService.findAll();}
+
+    @PreAuthorize("hasAnyAuthority('User Management')")
     @RequestMapping(value = "/usermanagement/privileges",method = RequestMethod.GET)
     public String showPrivileges(final ModelMap model){
         model.addAttribute("allPrivileges",privilegeService.findAll());
         return "/Privileges/privilege-list";
     }
 
+    @PreAuthorize("hasAnyAuthority('User Management')")
     @RequestMapping(value = "/usermanagement/privileges/put", method = RequestMethod.GET)
     public String ViewCreatePrivilege(final ModelMap model){
         model.addAttribute(new Privilege("",""));
         return "/Privileges/privilege-manage";
     }
 
+    @PreAuthorize("hasAnyAuthority('User Management')")
     @RequestMapping(value = "/usermanagement/privileges/{id}",method = RequestMethod.GET)
     public String viewEditPrivilege(@PathVariable long id, final ModelMap model){
         model.addAttribute("privilege",privilegeService.findById(id));
         return "/Privileges/privilege-manage";
     }
 
+    @PreAuthorize("hasAnyAuthority('User Management')")
     @RequestMapping(value = {"/usermanagement/privileges/","/usermanagement/privileges/{id}"},method = RequestMethod.POST)
     public String addPrivilege(@Valid Privilege privilege, BindingResult result, final ModelMap model) {
         if (result.hasErrors()) {
@@ -46,6 +55,17 @@ public class PrivilegeController {
         }
         if (privilege.getId() == null) {
             if (privilegeService.findByName(privilege.getName()).isPresent()) {
+                model.addAttribute("PrivilegeInUse", "Privilege " + privilege.getName() + " is already in use!");
+                return "/Privileges/privilege-manage";
+            }
+
+            privilegeService.save(privilege);
+            return "redirect:/usermanagement/privileges";
+        }
+
+        Privilege tempPrivilege = privilegeService.findById(privilege.getId()).orElse(null);
+        if(!tempPrivilege.getName().equals(privilege.getName())){
+            if(privilegeService.findByName(privilege.getName()).isPresent()){
                 model.addAttribute("PrivilegeInUse", "Privilege " + privilege.getName() + " is already in use!");
                 return "/Privileges/privilege-manage";
             }
@@ -57,6 +77,7 @@ public class PrivilegeController {
         return "redirect:/usermanagement/privileges";
     }
 
+    @PreAuthorize("hasAnyAuthority('User Management')")
     @RequestMapping(value = "/usermanagement/privileges/{id}/delete",method = RequestMethod.GET)
     public String deletePrivilege(@PathVariable long id, final ModelMap model){
         privilegeService.deleteById(id);
