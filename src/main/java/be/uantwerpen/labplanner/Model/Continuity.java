0532f3678d491345ea53fe2c0255c9e1d58aa164
Ondrej Bures <30869245+OBU5@users.noreply.github.com>
2020-03-29 18:03:01 +0200

    Added classes - Continuity.java - Experiment

---
+++ src/main/java/be/uantwerpen/labplanner/Model/Continuity.java
@@ -0,0 +1,61 @@
+package be.uantwerpen.labplanner.Model;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+// Enum that describes type of continuity
+enum ContinuityType {
+    EQUAL,      //desired time between steps has to be equal to selected time
+    MORE,       //desired time between steps has to be equal or longer than selected time
+    LESS        //desired time between steps has to be equal or shorter than selected time
+}
+
+@Entity
+public class Continuity extends AbstractPersistable<Long> {
+
+    private final ContinuityType DEFAULT_CONTINUITY_TYPE = ContinuityType.EQUAL;
+    private final int DEFAULT_HOURS = 1;
+    private final int DEFAULT_MINUTES = 0;
+
+    //type of continuity (can be EQUAL, MORE, LESS)
+    private ContinuityType continuityType;
+
+    //hours between current step and next step
+    private int hours;
+
+    //minutes between current step and next step
+    private int minutes;
+
+    //current step to which we set the continuity
+    @OneToOne
+    @JoinColumn(name = "currentStep", nullable = false)
+    private Step currentStep;
+
+    //step that comes after current step
+    @OneToOne
+    @JoinColumn(name = "nextStep", nullable = false)
+    private Step nextStep;
+
+    public Continuity() {
+    }
+
+    public Continuity(ContinuityType continuityType, int hours, int minutes, Step currentStep, Step nextStep) {
+        this.continuityType = continuityType;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.currentStep = currentStep;
+        this.nextStep = nextStep;
+    }
+
+    public Continuity(Step currentStep, Step nextStep) {
+        this.continuityType = DEFAULT_CONTINUITY_TYPE;
+        this.hours = DEFAULT_HOURS;
+        this.minutes = DEFAULT_MINUTES;
+        this.currentStep = currentStep;
+        this.nextStep = nextStep;
+    }
+}
