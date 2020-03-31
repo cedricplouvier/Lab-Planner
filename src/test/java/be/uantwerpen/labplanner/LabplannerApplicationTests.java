7f9977d1711e42070fa07a39853980836abab0a1
RubenJoosen <38206670+RubenJoosen@users.noreply.github.com>
2020-03-31 17:25:01 +0200

    Test import changed

--- src/test/java/be/uantwerpen/labplanner/LabplannerApplicationTests.java
+++ src/test/java/be/uantwerpen/labplanner/LabplannerApplicationTests.java
@@ -1,9 +1,26 @@
 package be.uantwerpen.labplanner;
 
-import org.junit.jupiter.api.Test;
+import be.uantwerpen.labplanner.Controller.PrivilegeController;
+import be.uantwerpen.labplanner.Service.OwnPrivilegeService;
+import be.uantwerpen.labplanner.common.model.users.Privilege;
+import be.uantwerpen.labplanner.common.service.users.RoleService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 public class LabplannerApplicationTests {
@@ -16,4 +33,5 @@ public class LabplannerApplicationTests {
         int id = 0;
         assertEquals(id,0);
     }
+
 }
