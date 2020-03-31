ef989eabfd83f352bf9b9daa9beeb0bd89f9fe88
RubenJoosen <38206670+RubenJoosen@users.noreply.github.com>
2020-03-31 17:01:36 +0200

    basic appl test changed back 2.0

--- src/test/java/be/uantwerpen/labplanner/LabplannerApplicationTests.java
+++ src/test/java/be/uantwerpen/labplanner/LabplannerApplicationTests.java
@@ -3,9 +3,17 @@ package be.uantwerpen.labplanner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class LabplannerApplicationTests {
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void extraCheck(){
+        int id = 0;
+        assertEquals(id,0);
     }
 }
