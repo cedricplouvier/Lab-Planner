d78d2a4779f92bd84df7b55c04c1ef95a32cd23d
Timo <timo.nelen@student.uantwerpen.be>
2020-04-07 15:20:55 +0200

    fix broken import in stockcontroler

--- src/main/java/be/uantwerpen/labplanner/Controller/StockController.java
+++ src/main/java/be/uantwerpen/labplanner/Controller/StockController.java
@@ -25,7 +25,6 @@ import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.*;
 
-import static jdk.nashorn.internal.objects.Global.print;
 
 @Controller
 public class StockController {
