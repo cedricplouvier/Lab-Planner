3aa2e6908062950476d2894adc0e6e378496325f
jaimievranckx <jaimievranckx21@gmail.com>
2020-05-04 14:07:49 +0200

    implement gradient

--- src/main/resources/static/js/userCalendar.js
+++ src/main/resources/static/js/userCalendar.js
@@ -55,7 +55,35 @@ function ScheduleInfo() {
         }
     };
 }
+function RGBAToHexA(r,g,b,a) {
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    a = Math.round(a * 255).toString(16);
 
+    if (r.length == 1)
+        r = "0" + r;
+    if (g.length == 1)
+        g = "0" + g;
+    if (b.length == 1)
+        b = "0" + b;
+    if (a.length == 1)
+        a = "0" + a;
+
+    return "#" + r + g + b + a;
+}
+function calculateGradient(colorHex,gradientNumber) {
+    var radix = 16;
+    var r = parseInt(colorHex.slice(1, 3), radix),
+        g = parseInt(colorHex.slice(3, 5), radix),
+        b = parseInt(colorHex.slice(5, 7), radix),
+        a = parseInt(colorHex.slice(7, 9), radix) / 255 || 1;
+    let red = r + (gradientNumber*0.5) * (255 - r);
+    let green =g + (gradientNumber*0.5) * (255 - g);
+    let blue = b + (gradientNumber*0.5) * (255 - b);
+
+    return RGBAToHexA(red,green,blue,a);
+}
 function generateSchedule(viewName, renderStart, renderEnd) {
     ScheduleList = [];
     steps.forEach(function (step) {
@@ -71,10 +99,16 @@ function generateSchedule(viewName, renderStart, renderEnd) {
         schedule.isReadOnly = false;
         schedule.start = new Date(step['start'] + 'T' + step['startHour']);
         schedule.end = new Date(step['end'] + 'T' + step['endHour']);
+        let gradientNumber=0;
+        for(let current = 0;current <ScheduleList.length;current++){
+            if(ScheduleList[current].calendarId=== calendar.id){
+                gradientNumber++;
+            }
+        }
         schedule.color = calendar.color;
-        schedule.bgColor = calendar.bgColor;
-        schedule.dragBgColor = calendar.dragBgColor;
-        schedule.borderColor = calendar.borderColor;
+        schedule.bgColor = calculateGradient(calendar.bgColor,gradientNumber);
+        schedule.dragBgColor = calculateGradient(calendar.dragBgColor,gradientNumber);
+        schedule.borderColor = calculateGradient(calendar.borderColor,gradientNumber);
         schedule.category = 'time';
 
         ScheduleList.push(schedule);
