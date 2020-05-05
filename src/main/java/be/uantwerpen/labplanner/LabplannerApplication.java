52cdf43fbb551ac2626ada84da82d10a6bf2b913
RubenJoosen <38206670+RubenJoosen@users.noreply.github.com>
2020-05-05 15:16:16 +0200

    custom login page

--- src/main/java/be/uantwerpen/labplanner/LabplannerApplication.java
+++ src/main/java/be/uantwerpen/labplanner/LabplannerApplication.java
@@ -17,10 +17,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -92,5 +95,30 @@ public class LabplannerApplication extends WebMvcConfigurerAdapter {
                 NoOpPasswordEncoder.getInstance();
     }
 
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+    @Order(99)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+        @Override
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("/login").permitAll().anyRequest()
+                    .fullyAuthenticated()
+                    .and().formLogin().loginPage("/login")
+                    .failureUrl("/login?error")
+                    .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+                    //.and().exceptionHandling().accessDeniedPage("/access?error");
+        }
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    { registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/access").setViewName("access");
+    }
+//
+//
 
 }
