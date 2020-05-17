360a8e67f11c549fd05b0df0ec2d700f157dfbf4
Ondřej Bureš <30869245+OBU5@users.noreply.github.com>
2020-05-17 14:04:43 +0200

    fixed General error when user is redirected on book an experiment page or when edit experiment

--- src/main/java/be/uantwerpen/labplanner/Model/Experiment.java
+++ src/main/java/be/uantwerpen/labplanner/Model/Experiment.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.labplanner.Model;
 
 import be.uantwerpen.labplanner.common.model.users.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ public class Experiment extends AbstractPersistable<Long> {
 
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "user")
     private User user;
 
